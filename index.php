5716459a0bb144da8d8441b01c0be4266d2e7af6
Mercy <m.hehuat@mac.com>
2017-10-14 12:35:50 +0200

    version two

--- index.php
+++ index.php
@@ -1,15 +1,3 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <h1></h1>
-    <h2></h2>
-    <p></p>
-  </body>
-</html>
 <?php
 echo '<!DOCTYPE html>';
 echo '<html>';
