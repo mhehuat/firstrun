2fa06a123a7c87173dde56c0d0248171827ce23d
Mercy <m.hehuat@mac.com>
2017-10-14 14:06:34 +0200

    Added the contact template

---
+++ contacts.php
@@ -0,0 +1,13 @@
+<?php
+echo '<!DOCTYPE html>';
+echo '<html>';
+  echo '<head>';
+    echo '<meta charset="utf-8">';
+    echo '<title>Testing contacts Git</title>';
+  echo '</head>';
+  echo '<body>';
+    echo '<h1>Version One</h1>';
+    echo '<h2>test line</h2>';
+    echo '<p>some longer text made longer</p>';
+  echo '</body>';
+echo '</html>';
