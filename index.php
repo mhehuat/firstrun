ead24b5653b1ce0dbbb9129c79e5821fca5ceb03
Mercy <m.hehuat@mac.com>
2017-10-14 12:22:03 +0200

    version one

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1></h1>
+    <h2></h2>
+    <p></p>
+  </body>
+</html>
+<?php
+echo '<!DOCTYPE html>';
+echo '<html>';
+  echo '<head>';
+    echo '<meta charset="utf-8">';
+    echo '<title>Testing Git</title>';
+  echo '</head>';
+  echo '<body>';
+    echo '<h1>Version One</h1>';
+    echo '<h2>test line</h2>';
+    echo '<p>some longer text</p>';
+  echo '</body>';
+echo '</html>';
